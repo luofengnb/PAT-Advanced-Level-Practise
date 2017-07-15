bb5c19fe17aeafbef74e1f77359c105579f0cb28
luofengnb <2677281339@qq.com>
2017-07-15 12:02:43 +0800

    一次通过
    优先队列优先级的自定义既可以通过自定义结构体内的cmp函数实现，也可以重载‘<’来实现。

--- 1017.cpp
+++ 1017.cpp
@@ -2,38 +2,52 @@
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define ll long long
 using namespace std;
+int closeTime=17*60*60,openTime=8*60*60;
 struct peo{
-	int hour,minute,second,processTime;
-	bool operator <(const peo &b)const{
-	if(hour==b.hour){
-		if(minute==b.minute)
-			return second>b.second;
-		return minute>b.minute;
+	int arriveTime,startTime,processTime,doneTime;
+};
+struct cmp1{	bool operator()(const peo &a,const peo &b)const{
+		return a.arriveTime>b.arriveTime;
 	}
-	return hour>b.hour;
+};
+struct cmp2{	bool operator()(const peo &a,const peo &b)const{
+		return a.doneTime>b.doneTime;
 	}
 };
 int main(){_
 	#ifdef INPUT
 	freopen("data.in","r",stdin);
 	#endif
-    int peopleNum,windowsNum;
+    unsigned int peopleNum,windowsNum;
 	cin>>peopleNum>>windowsNum;
-	peo p;
-	priority_queue<peo> pq;
-	char useless; 
-	for(int i=0;i<peopleNum;i++){
-		cin>>p.hour>>useless>>p.minute>>useless>>p.second>>p.processTime;
-		pq.push(p);
+	priority_queue<peo,vector<peo>,cmp1> wait;
+	priority_queue<peo,vector<peo>,cmp2> service;
+	int hour,minute,second;
+	char useless;
+	for(unsigned int i=0;i<peopleNum;i++){
+		peo p;
+		cin>>hour>>useless>>minute>>useless>>second>>p.processTime;
+		p.arriveTime=hour*60*60+minute*60+second;
+		if(p.arriveTime>closeTime)	continue;
+		wait.push(p);
 	}
-/*
-while(!pq.empty()){
-		cout<<setfill('0')<<setw(2)<<pq.top().hour<<":"<<setw(2)<<pq.top().minute<<":"<<setw(2)<<pq.top().second<<endl;
-		pq.pop();
+	int serviceNum=wait.size(),sumWaitTime=0;
+	int leaveTime=openTime;
+	while(!wait.empty()){//等待队列未空 
+		while(service.size()<windowsNum){//窗口没满
+			if(!wait.empty()){
+				peo current=wait.top();
+				current.startTime=max(leaveTime,current.arriveTime);//开始服务的时间=MAX（自己到达的时间 ,窗口空出的时间） 
+				current.doneTime=current.startTime+current.processTime*60;
+				sumWaitTime+=current.startTime-current.arriveTime;
+				wait.pop();			
+				service.push(current);
+			}
+			else break;
+		}
+		leaveTime=service.top().doneTime;
+		service.pop();//每次服务完一个人 
 	}
-*/	
-//	sort(p.begin(),p.end(),cmp);
-	
-    
+    cout<<setprecision(1)<<fixed<<sumWaitTime*1.0/serviceNum/60;
     return 0;
 }
