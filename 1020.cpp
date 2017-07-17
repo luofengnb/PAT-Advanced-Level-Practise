e83c70c981a3d5ebaf09ad69d9f3736ed7f7d8f4
luofengnb <2677281339@qq.com>
2017-07-17 08:54:03 +0800

    两种方式都可以
    建树无论是否返回Node*还是空，queue中无论是Node还是Node*，都是可以的。

--- 1020.cpp
+++ 1020.cpp
@@ -5,18 +5,17 @@ using namespace std;
 int postorder[40]={0},inorder[40]={0};
 struct Node{
 	int data;
-	struct Node *right,*left;
+	struct Node *right=NULL,*left=NULL;
 };
-Node *createTree(int postleft,int postright,int inleft,int inright){//这里返回void为什么不行？ 
-	if(postleft>postright)	return NULL;
-	Node* tree=new Node;
+void createTree(Node*& tree,int postleft,int postright,int inleft,int inright){//建树函数返回void,BFS的队列元素类型是Node
+	if(postleft>postright)	return;
+	tree=new Node;
 	tree->data=postorder[postright];
 	int pos;
 	for(pos=inleft;pos<=inright;pos++)
 		if(inorder[pos]==tree->data)	break;
-	tree->left=createTree(postleft,postleft+pos-inleft-1,inleft,pos-1);
-	tree->right=createTree(postleft+pos-inleft,postright-1,pos+1,inright);
-	return tree;
+	createTree(tree->left,postleft,postleft+pos-inleft-1,inleft,pos-1);
+	createTree(tree->right,postleft+pos-inleft,postright-1,pos+1,inright);
 }  
 int main(){_
 	#ifdef INPUT
@@ -29,20 +28,20 @@ int main(){_
     for(int i=1;i<N+1;i++)
     	cin>>inorder[i];
     Node *root=NULL;
-    root=createTree(1,N,1,N);
-    queue<Node*> q;
-    q.push(root);
+    createTree(root,1,N,1,N);
+    queue<Node> q;
+    q.push(*root);
     int firstOutputFlag=1;
     while(!q.empty()){	
     	if(firstOutputFlag){
-    		cout<<q.front()->data;
+    		cout<<q.front().data;
     		firstOutputFlag=0;
 		}
-    	else  cout<<" "<<q.front()->data;
-    	if(q.front()->left)
-    		q.push(q.front()->left);
-    	if(q.front()->right)
-    		q.push(q.front()->right);
+    	else  cout<<" "<<q.front().data;
+    	if(q.front().left)
+    		q.push(*(q.front().left));
+    	if(q.front().right)
+    		q.push(*(q.front().right));
     	q.pop();
 	}
     return 0;
